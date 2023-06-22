c08888313813ceb3a51af8ea61a697a7c85df1b8
Filipe Bacof <filipebacof@gmail.com>
2023-06-22 08:41:12 -0300

    FEAT: shadow on the links

--- src/app/components/HomeNavigationLinksMedium.tsx
+++ src/app/components/HomeNavigationLinksMedium.tsx
@@ -3,20 +3,35 @@ import Link from 'next/link'
 export default function HomeNavigationLinksMedium() {
   return (
     <nav className="hidden items-center justify-center text-lg font-bold text-blue-900 md:flex md:flex-row md:gap-4 lg:gap-8">
-      <Link href={'/about'}>
-        <span>Sobre Mim</span>
+      <Link
+        className="rounded-3xl hover:bg-[rgba(0,0,0,0.4)] hover:shadow-[0px_0px_20px_2px_rgba(0,0,0,0.74)]"
+        href={'/about'}
+      >
+        <span className="hover:underline">Sobre Mim</span>
       </Link>
-      <Link href={'/contact'}>
-        <span>Contato</span>
+      <Link
+        className="rounded-3xl hover:bg-[rgba(0,0,0,0.4)] hover:shadow-[0px_0px_20px_2px_rgba(0,0,0,0.74)]"
+        href={'/contact'}
+      >
+        <span className="hover:underline">Contato</span>
       </Link>
-      <Link href={'/projects'}>
-        <span>Projetos</span>
+      <Link
+        className="rounded-3xl hover:bg-[rgba(0,0,0,0.4)] hover:shadow-[0px_0px_20px_2px_rgba(0,0,0,0.74)]"
+        href={'/projects'}
+      >
+        <span className="hover:underline">Projetos</span>
       </Link>
-      <Link href={'/techs'}>
-        <span>Tecnologias</span>
+      <Link
+        className="rounded-3xl hover:bg-[rgba(0,0,0,0.4)] hover:shadow-[0px_0px_20px_2px_rgba(0,0,0,0.74)]"
+        href={'/techs'}
+      >
+        <span className="hover:underline">Tecnologias</span>
       </Link>
-      <Link href={'/oportunities'}>
-        <span>Oportunidades</span>
+      <Link
+        className="rounded-3xl hover:bg-[rgba(0,0,0,0.4)] hover:shadow-[0px_0px_20px_2px_rgba(0,0,0,0.74)]"
+        href={'/oportunities'}
+      >
+        <span className="hover:underline">Oportunidades</span>
       </Link>
     </nav>
   )
